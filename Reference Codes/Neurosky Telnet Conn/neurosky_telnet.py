fffb0a58ce169ba208ca21bd5d7fa49552a682ef
Chirag Bellara <chirab99@gmail.com>
2019-10-06 10:44:08 +0530

    test output

--- Reference Codes/Neurosky Telnet Conn/neurosky_telnet.py
+++ Reference Codes/Neurosky Telnet Conn/neurosky_telnet.py
@@ -19,8 +19,10 @@ if len(sys.argv)>1:
 eSenseDict={'attention':0, 'meditation':0}
 waveDict={'lowGamma':0, 'highGamma':0, 'highAlpha':0, 'delta':0, 'highBeta':0, 'lowAlpha':0, 'lowBeta':0, 'theta':0}
 signalLevel=0
-csvFile = open('Anurag_output.csv','w')
+csvFile = open('Pranali_outputb.csv','w')
+csv1File = open('Pranali_outpute.csv','w')
 count=0
+c1=0
 while i<100:
     blinkStrength=0
     line=tn.read_until(b'\r')
@@ -28,16 +30,33 @@ while i<100:
         line = line.decode('ascii')
         #print(type(line))
         print(line)
-        if line[3] == 'b':
+        if line[2] == 'b':
             dict=json.loads(line)
+            #print(dict)
             if count==0:
                 header = dict.keys()
+                csvwriter=csv.writer(csvFile)
                 csvwriter.writerow(header)
                 count+=1
             #print(dict['blinkStrength'],type(dict))        
             csvwriter=csv.writer(csvFile)
             csvwriter.writerow(dict.values())
+        elif line[2] == 'e':
+            dict=json.loads(line)
+            #print(dict)
+            dict1=dict['eSense']
+            d2=dict['eegPower']
+            if c1==0:
+                header = dict1.keys()
+                h = d2.keys()
+                csvwriter=csv.writer(csv1File)
+                csvwriter.writerow([header,h])
+                c1+=1
+        
+            csvwriter=csv.writer(csv1File)
+            csvwriter.writerow([dict1.values(),d2.values()])
 csvFile.close()
+csv1File.close()
         
 tn.close()
 outfptr.close()
