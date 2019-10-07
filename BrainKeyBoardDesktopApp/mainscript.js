a7869b138314f301bc630c05d0adebdebf3523cd
Chirag Bellara <chirab99@gmail.com>
2019-10-07 16:55:34 +0530

    javascript for python and electron connection

--- BrainKeyBoardDesktopApp/mainscript.js
+++ BrainKeyBoardDesktopApp/mainscript.js
@@ -3,62 +3,147 @@ var secInternal = null;
 var thirdInternal = null;
 let divrow = "firstrow";
 let old = "fifthrow";
-var firstRowChild = []
-var current = null;
-var second = null;
+var firstRowChild = [];
+var secRowChild = [];
+// var current = null;
+// var second = null;
 var eachButton = []
-var accept = 0;
-var data = null
+// var accept = 0;
+// var data = null
 
-function reset(){
-    internal = null;
-    secInternal = null;
-    thirdInternal = null;
-    divrow = "firstrow";
-    old = "fifthrow";
-    firstRowChild = []
-    current = null;
-    second = null;
-    eachButton = []
-    accept = 0;
-    data= null
+// function reset(){
+//     clearInterval(thirdInternal);
+//     document.getElementById(data).style.backgroundColor=null;
+//     document.getElementById(data).style.boxShadow=null;
+//     accept = 1;
+//     document.getElementById("textscreen").value+=data
+//     reset();
+//     first_opt();
+//     internal = null;
+//     secInternal = null;
+//     thirdInternal = null;
+//     divrow = "firstrow";
+//     old = "fifthrow";
+//     firstRowChild = []
+//     current = null;
+//     second = null;
+//     eachButton = []
+//     accept = 0;
+//     data= null
+// }
+// // function myFunction(){
+// //     if(accept===1){
+        
+// //         firstChild();
+// //     }
+// //     else if(accept===2){
+        
+// //         eachBtn();
+
+// //     }
+// //     else if(accept===3){
+        
+// //     }
+// // }
+
+// //Button selection
+// function eachBtn(){
+//     clearInterval( secInternal );
+//     document.getElementById(second).style.backgroundColor=null;
+//     document.getElementById(second).style.boxShadow=null;
+//     var children =document.getElementById(second).children;
+//     for(i=0; i<children.length;i++ ){
+//         eachButton.push(children[i].id);
+//     }
+//     console.log(eachButton);
+//     if (! thirdInternal){
+//         accept =3
+//         var count =0
+//         var btn = null
+//         data = eachButton[0]
+//         thirdInternal = setInterval(function(){
+//             document.getElementById(data).style.backgroundColor=null;
+//             document.getElementById(data).style.boxShadow=null;
+//             btn= count%3
+//             count= count+1;
+//             data = eachButton[btn]
+//             console.log(data)
+//             document.getElementById(data).style.backgroundColor="green";
+//             document.getElementById(data).style.boxShadow="0 15px 8px -6px #666";
+//         },2000);
+//     }
+// }
+
+// // Two row selection
+// function firstChild(){
+//     clearInterval( internal );
+//     document.getElementById(old).style.backgroundColor=null;
+//     document.getElementById(old).style.boxShadow=null;
+//     var children =document.getElementById(old).children;
+//     for(i=0; i<children.length;i++ ){
+//         firstRowChild.push(children[i].id);
+//     }
+//     current = firstRowChild[0];
+//     second = firstRowChild[1];
+//     if(! secInternal){
+//         accept=2
+//         secInternal = setInterval(function(){
+//             document.getElementById(second).style.backgroundColor=null;
+//             document.getElementById(current).style.backgroundColor="blue";
+//             document.getElementById(second).style.boxShadow=null
+//             document.getElementById(current).style.boxShadow="0 15px 8px -6px #666";
+
+//             if(true){
+//                 var n = current
+//                 current = second
+//                 second = n
+//             }
+//         }, 3000);
+//     }
+    
+// }
+
+
+function callFromPython(){
+    clearInterval(internal)
+    console.log("testing"+old)
+    document.getElementById(old).style.backgroundColor=null;
+    document.getElementById(old).style.boxShadow=null;
+    var children =document.getElementById(old).children;
+    for(i=0; i<children.length;i++ ){
+        firstRowChild.push(children[i].id);
+    }
+
+    old = firstRowChild[0];
+    divrow = firstRowChild[1];
+    if(! secInternal){
+        accept=2
+        secInternal = setInterval(function(){
+            document.getElementById(old).style.backgroundColor=null;
+            document.getElementById(divrow).style.backgroundColor="blue";
+            document.getElementById(old).style.boxShadow=null
+            document.getElementById(divrow).style.boxShadow="0 15px 8px -6px #666";
+
+            if(true){
+                var n = divrow
+                divrow = old
+                old = n
+            }
+            
+        }, 2000);
+    }
+
 }
-function myFunction(event){
-    if(event.keyCode === 13 && accept===1){
-        clearInterval( internal );
-        document.getElementById(old).style.backgroundColor=null;
-        document.getElementById(old).style.boxShadow=null;
-        var children =document.getElementById(old).children;
-        for(i=0; i<children.length;i++ ){
-            firstRowChild.push(children[i].id);
-        }
-        firstChild();
-    }
-    else if(event.keyCode === 13 && accept===2){
-        clearInterval( secInternal );
-        document.getElementById(second).style.backgroundColor=null;
-        document.getElementById(second).style.boxShadow=null;
-        var children =document.getElementById(second).children;
-        for(i=0; i<children.length;i++ ){
-            eachButton.push(children[i].id);
-        }
-        console.log(eachButton);
-        eachBtn();
 
+function callFromPython2(){
+    clearInterval( secInternal );
+    document.getElementById(old).style.backgroundColor=null;
+    document.getElementById(old).style.boxShadow=null;
+    var children =document.getElementById(old).children;
+    for(i=0; i<children.length;i++ ){
+        eachButton.push(children[i].id);
     }
-    else if(event.keyCode === 13 && accept===3){
-        clearInterval(thirdInternal);
-        document.getElementById(data).style.backgroundColor=null;
-        document.getElementById(data).style.boxShadow=null;
-        accept = 1;
-        document.getElementById("textscreen").value+=data
-        reset();
-        first_opt();
-    }
-}
-
-//Button selection
-function eachBtn(){
+    console.log("Second Call"+eachButton);
     if (! thirdInternal){
         accept =3
         var count =0
@@ -75,36 +160,31 @@ function eachBtn(){
             document.getElementById(data).style.boxShadow="0 15px 8px -6px #666";
         },2000);
     }
+
 }
 
-// Two row selection
-function firstChild(){
-    current = firstRowChild[0];
-    second = firstRowChild[1];
-    if(! secInternal){
-        accept=2
-        secInternal = setInterval(function(){
-            document.getElementById(second).style.backgroundColor=null;
-            document.getElementById(current).style.backgroundColor="blue";
-            document.getElementById(second).style.boxShadow=null
-            document.getElementById(current).style.boxShadow="0 15px 8px -6px #666";
-
-            if(true){
-                var n = current
-                current = second
-                second = n
-            }
-        }, 3000);
-    }
-    
+function callFromPython3(){
+    clearInterval(thirdInternal);
+    document.getElementById(data).style.backgroundColor=null;
+    document.getElementById(data).style.boxShadow=null;
+    document.getElementById("textscreen").value+=data
+    internal = null;
+    secInternal = null;
+    thirdInternal = null;
+    divrow = "firstrow";
+    old = "fifthrow";
+    firstRowChild = []
+    eachButton = []
+    data= null
+    first_opt();
 }
 
-// Single row selection
+
+
 function first_opt(){
     if (! internal){
         accept =1
         internal = setInterval(function(){ 
-            console.log(divrow)
             document.getElementById(old).style.backgroundColor=null;
             document.getElementById(divrow).style.backgroundColor="blue";
             document.getElementById(old).style.boxShadow=null
--- BrainKeyBoardDesktopApp/testpython.py
+++ BrainKeyBoardDesktopApp/testpython.py
@@ -1,8 +1,9 @@
 import sys
 import time 
+import random
 a =  sys.argv[0]
 print(a)
 while(1):
     print("hello from python")
-    time.sleep(2)
+    time.sleep(random.choice([1,2,3, 4,5,6,7, 8,9,10]))
     sys.stdout.flush()
