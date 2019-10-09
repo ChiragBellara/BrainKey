b22630e065c654c2f215a7c658316baf2979bf60
Raj-Madheshia <rajsmadheshia@gmail.com>
2019-10-09 22:00:03 +0530

    Single Blink Button click completed

--- BrainKeyBoardDesktopApp/mainscript.js
+++ BrainKeyBoardDesktopApp/mainscript.js
@@ -11,42 +11,6 @@ var eachButton = []
 // var accept = 0;
 // var data = null
 
-<<<<<<< HEAD
-function reset(){
-    internal = null;
-    secInternal = null;
-    thirdInternal = null;
-    divrow = "firstrow";
-    old = "fifthrow";
-    firstRowChild = []
-    current = null;
-    second = null;
-    eachButton = []
-    accept = 0;
-    data= null
-}
-function myFunction(){
-    if(accept===1){
-        clearInterval( internal );
-        document.getElementById(old).style.backgroundColor=null;
-        document.getElementById(old).style.boxShadow=null;
-        var children =document.getElementById(old).children;
-        for(i=0; i<children.length;i++ ){
-            firstRowChild.push(children[i].id);
-        }
-        firstChild();
-    }
-    else if(accept===2){
-        clearInterval( secInternal );
-        document.getElementById(second).style.backgroundColor=null;
-        document.getElementById(second).style.boxShadow=null;
-        var children =document.getElementById(second).children;
-        for(i=0; i<children.length;i++ ){
-            eachButton.push(children[i].id);
-        }
-        console.log(eachButton);
-        eachBtn();
-=======
 // function reset(){
 //     clearInterval(thirdInternal);
 //     document.getElementById(data).style.backgroundColor=null;
@@ -128,7 +92,6 @@ function myFunction(){
 //             document.getElementById(current).style.backgroundColor="blue";
 //             document.getElementById(second).style.boxShadow=null
 //             document.getElementById(current).style.boxShadow="0 15px 8px -6px #666";
->>>>>>> e19f03da165d842a12cb494a9f4208adefbe8e28
 
 //             if(true){
 //                 var n = current
@@ -149,16 +112,6 @@ function callFromPython(){
     for(i=0; i<children.length;i++ ){
         firstRowChild.push(children[i].id);
     }
-<<<<<<< HEAD
-    else if(accept===3){
-        clearInterval(thirdInternal);
-        document.getElementById(data).style.backgroundColor=null;
-        document.getElementById(data).style.boxShadow=null;
-        accept = 1;
-        document.getElementById("textscreen").value+=data
-        reset();
-        first_opt();
-=======
 
     old = firstRowChild[0];
     divrow = firstRowChild[1];
@@ -177,7 +130,6 @@ function callFromPython(){
             }
             
         }, 2000);
->>>>>>> e19f03da165d842a12cb494a9f4208adefbe8e28
     }
 
 }
@@ -207,28 +159,6 @@ function callFromPython2(){
         },2000);
     }
 
-<<<<<<< HEAD
-// Two row selection
-function firstChild(){
-    current = firstRowChild[0];
-    second = firstRowChild[1];
-    if(! secInternal){
-        accept=2
-        secInternal = setInterval(function(){
-            document.getElementById(second).style.backgroundColor=null;
-            document.getElementById(current).style.backgroundColor="blue";
-            document.getElementById(second).style.boxShadow=null
-            document.getElementById(current).style.boxShadow="0 15px 8px -6px #666";
-            console.log(document.getElementById(second).value)
-            if(true){
-                var n = current
-                current = second
-                second = n
-            }
-        }, 3000);
-    }
-    
-=======
 }
 
 function callFromPython3(){
@@ -245,7 +175,6 @@ function callFromPython3(){
     eachButton = []
     data= null
     first_opt();
->>>>>>> e19f03da165d842a12cb494a9f4208adefbe8e28
 }
 
 
@@ -281,11 +210,6 @@ function first_opt(){
                 old =  "fifthrow"
                 divrow = "firstrow"
             }
-<<<<<<< HEAD
-            console.log("mainscript")
-        }, 3000);
-=======
         }, 2000);
->>>>>>> e19f03da165d842a12cb494a9f4208adefbe8e28
     }
 }
